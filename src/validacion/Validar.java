5a0256ea9454cf4dc4bd843bbfda6166b7127fb0
ANGELA-PCTACENS\Angela <angelavazquezdominguez@gmail.com>
2019-03-16 20:13:11 +0100

    Algunos metodos de validacion creados

---
+++ src/validacion/Validar.java
@@ -0,0 +1,49 @@
+package validacion;
+import enumerados.Tipo;
+import gestion.GestionPokemon;
+
+import java.util.Scanner;
+public class Validar {
+    /*
+    * INTERFAZ
+    * Signatura: public String ejecutar()
+    * Comentario: pide y valida si desea ejecutar el programa
+    * Precondiciones:
+    * Entradas:
+    * Salidas: String que es "si" si quiere ejecutar y "no" si no quiere
+    * Postcondiciones: se devolvera asociado al nombre si desea ejecutar o no
+    * */
+    public String ejecutar(){
+        Scanner sc = new Scanner(System.in);
+        String entrada;
+        do{
+            System.out.println("¿Desea ejecutar el programa de Tipos Pokemon? Escribe 'Si' o 'No': ");
+            entrada = sc.next();
+        }while( (!entrada.toUpperCase().equals("SI") && !entrada.toUpperCase().equals("NO")) );
+        return entrada;
+    }
+
+    /*
+     * INTERFAZ
+     * Signatura: public Tipo tipo()
+     * Comentario: pide y valida un tipo pokemon
+     * Precondiciones:
+     * Entradas:
+     * Salidas: Tipo pokemon
+     * Postcondiciones: se devolvera asociado al nombre un Tipo
+     * */
+    public Tipo tipo(){
+        Scanner sc = new Scanner(System.in);
+        GestionPokemon gestion = new GestionPokemon();
+        int tipo=0;
+        do{
+            System.out.println("Selecciona un tipo pokemon: ");
+            gestion.imprimirTipos();
+            tipo = sc.nextInt();
+        }while(tipo < 0 || tipo > 18);
+    return gestion.obtenerTipodePosicion(tipo);
+    }
+
+
+
+}
